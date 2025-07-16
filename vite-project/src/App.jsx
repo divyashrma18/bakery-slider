63484a98105a67f6e3a48da3a8471b44ce334cf6
divyashrma18 <dvsharma3143@gmail.com>
2025-07-16 15:10:42 +0530

    complete

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Hero from './components/Hero'
+import HeroSlide from './components/HeroSlide'
 
 function App() {
 
   return (
     <>
     <Hero />
+    {/* <HeroSlide /> */}
     </>
   )
 }
